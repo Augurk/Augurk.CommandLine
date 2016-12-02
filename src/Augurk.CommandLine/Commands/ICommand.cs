00253689cc2dee90988ffbf942276de605fe24f1
Jonathan Mezach <jonathanm@infosupport.com>
2016-12-02 21:43:04 +0100

    Initial move of the code

---
+++ src/Augurk.CommandLine/Commands/ICommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Augurk.CommandLine.Commands
+{
+    /// <summary>
+    /// Describes the interface for executable commands.
+    /// </summary>
+    internal interface ICommand
+    {
+        /// <summary>
+        /// Called when the command should be executed.
+        /// </summary>
+        void Execute();
+    }
+}
---
+++ src/Augurk.CommandLine/Commands/ICommandMetadata.cs
@@ -0,0 +1,13 @@
+﻿namespace Augurk.CommandLine.Commands
+{
+    /// <summary>
+    /// Describes the required metadata for a command.
+    /// </summary>
+    public interface ICommandMetadata
+    {
+        /// <summary>
+        /// Gets the verb of the command.
+        /// </summary>
+        string Verb { get; }
+    }
+}
